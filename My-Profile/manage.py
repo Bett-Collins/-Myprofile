5a1683512cc19ed72e8c0e127bca8f31ae3d8844
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-03-08 00:41:20 +0300

    it will add class user and model

---
+++ My-Profile/manage.py
@@ -0,0 +1,13 @@
+from app import create_app,db
+import app
+# .....
+from app.models import User
+from flask_script import Manager
+# ...
+#....
+manager = Manager(app)
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+if __name__ == '__main__':
+    manager.run()
