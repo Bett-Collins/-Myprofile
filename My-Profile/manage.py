aaa848055fb0b097ef7fe37b3783eb76440159bf
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-03-08 02:34:03 +0300

    it will start user authentication

--- My-Profile/manage.py
+++ My-Profile/manage.py
@@ -1,15 +1,31 @@
 from app import create_app,db
 from app.models import User,Role
 from flask_script import Manager, Server
+from  flask_migrate import Migrate,MigrateCommand
 # ...
 #....
+
+#.......
 app = create_app('development')
+
 manager = Manager(app)
 manager.add_command('server', Server)
 
-@manager.shell
-def make_shell_context():
-    return dict(app = app,db = db,User = User, Role = Role )
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+
+@manager.command
+def test():
+    
+    
+
+    @manager.shell
+    
+    def make_shell_context():
+        
+     return dict(app = app,db = db,User = User, Role = Role )
 if __name__ == '__main__':
     manager.run()
 
