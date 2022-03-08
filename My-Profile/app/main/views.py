f503351f7accdca81b1fffc8412b71e50e3ddbbe
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-03-08 03:19:30 +0300

    it will add flask login

--- My-Profile/app/main/views.py
+++ My-Profile/app/main/views.py
@@ -1,9 +1,12 @@
 from . import main
 from flask import render_template
+from flask_login import login_required
 
-
-@main.route("/")
-def index():
+#....
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@login_required
+def new_review(id):
+#....
     
     message = 'Hello world'
     
--- My-Profile/app/models.py
+++ My-Profile/app/models.py
@@ -1,8 +1,18 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
-
+from flask_login import UserMixin
+from . import login_manager
 #...
 # ....
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(255))
+
 class Role(db.Model):
     __tablename__ = 'roles'
 
@@ -34,3 +44,11 @@ def password(self, password):
 
 def verify_password(self,password):
             return check_password_hash(self.pass_secure,password)
+        
+        #....
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+#....
