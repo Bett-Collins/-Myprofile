581ba9a0c16dcd5d9c8f8ee25d4a950dcf2a1109
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-03-08 04:41:03 +0300

    it will create user page

--- My-Profile/manage.py
+++ My-Profile/manage.py
@@ -2,6 +2,7 @@ from app import create_app,db
 from app.models import User,Role
 from flask_script import Manager, Server
 from  flask_migrate import Migrate,MigrateCommand
+from flask_bootstrap import Bootstrap
 # ...
 #....
 
@@ -14,7 +15,7 @@ manager.add_command('server', Server)
 
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
-
+bootstrap = Bootstrap(app)
 
 @manager.command
 def test():
