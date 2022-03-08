09ea66062d08db367ec1bde84d35149c106cdad7
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-03-08 05:07:25 +0300

    it will add sending email functionality

--- My-Profile/app/__init__.py
+++ My-Profile/app/__init__.py
@@ -4,8 +4,17 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 
+#........
+mail = Mail()
+
+def create_app(config_name):
+    app = Flask(__name__)
+    #........
+    mail.init_app(app)
+#........
 #.............
 photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
--- My-Profile/config.py
+++ My-Profile/config.py
@@ -5,8 +5,12 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:2020@localhost/profile'
     SECRET_KEY = 'Flask WTF Secret Key'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-
-
+#  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_arapbett1996@gmail.com")
+    MAIL_PASSWORD = os.environ.get("MAIL_3978737")
 class ProdConfig(Config):
     '''
     Production  configuration child class
